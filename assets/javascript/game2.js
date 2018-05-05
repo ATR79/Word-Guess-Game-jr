7e469bb4ae207debe0087d002a31f03eda0efda6
Anna Rutherford <anna.t.rutherford@gmail.com>
2018-05-04 23:52:43 -0400

    code along attempt

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -1,10 +1,57 @@
 //array of bodyParts
+var bodyParts = ["nose", "eyes", "feet", "hands", "chest", "ears", "mouth",
+    "teeth", "hair", "cheek", "chin", "knee cap", "wrist", "shoulder", "elbow",
+    "toe nails"
+];
 
-var bodyParts = ["nose", "eyes", "feet", "hands", "chest", "ears", "mouth", 
-"teeth", "hair", "cheek", "chin", "knee cap", "wrist", "shoulder", "elbow", 
-"toe nails"];
+//Choose random word
+var randomWord = Math.floor(Math.random() * bodyParts.length);
+var rightPart = [];
+var wrongPart = [];
+var dashes = [];
+var choosenBodyParts = bodyParts[randomWord];
 
-let randomWord = Math.floor(Math.random() * bodyParts.length);
-let choosenBodyParts = bodyParts[randomWord];
 
+var docDashes = document.getElementsByClassName('dashes');
+
+//test
 console.log(choosenBodyParts);
+
+//Create dashes for length of word
+var createDashes = () => {
+    for (var i = 0; i < choosenBodyParts.length; i++) {
+        dashes.push('__');
+    }
+    return dashes;
+}
+console.log(createDashes());
+
+//Get users guess
+document.addEventListener('keypress', (event) => {
+    var keycode = event.keyCode;
+    var keyword = String.fromCharCode(keycode);
+//Is guess right?
+    if(choosenBodyParts.indexOf(keyword) > -1){
+    //If guess is right
+        rightPart.push(keyword);
+        docDashes[0].innerHTML = dashes.join('');
+    //Replace dashes with right letter
+        dashes[choosenBodyParts.indexOf(keyword)] = keyword;
+        if(dashes.join('') == choosenBodyParts){
+            alert('You Win!');
+        }
+        else {
+            wrongPart.push(keyword);
+            console.log(wrongPart);
+        }
+       
+    }
+});
+
+
+
+
+
+
+
+
